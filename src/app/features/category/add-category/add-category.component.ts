6d1bbd0c54a584b338c4bd5e2eaf0b9585f4dd68
Gayatri Gurav <gayatri.gurav86@gmail.com>
2024-03-30 15:09:35 -0500

    Added UI for displaying category list. Used Async pipe for observable subscription. Updated service for get api call. Also Added environment to add baseurl for api. Also used router direction to navigate from add categories to get categories.

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-reuest.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
 
-  constructor(){
+  private addCategorySubscription?: Subscription;
+
+  constructor(private categoryService: CategoryService ){
     this.model ={
       name:'',
       urlHandle:''
@@ -18,6 +22,15 @@ export class AddCategoryComponent {
   }
 
   onFormSubmit(){
+    this.addCategorySubscription = this.categoryService.addCategory(this.model)
+     .subscribe({
+      next: (response) => {
+       console.log('This was successful')
+      }
+     })
+  }
 
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 }
